c7b280b53b6075c875e3c67ce74b698502609775
EIOlmqV6v1owgJBEoqj5lK9p01SFE2THpxcOnvX <114466749+EIOlmqV6v1owgJBEoqj5lK9p01SFE2THpxcOnvX@users.noreply.github.com>
2022-10-11 20:00:37 +1000

    Update ThemeManager.lua

--- addons/ThemeManager.lua
+++ addons/ThemeManager.lua
@@ -5,7 +5,7 @@ local ThemeManager = {} do
 
 	ThemeManager.Library = nil
 	ThemeManager.BuiltInThemes = {
-		['Mainhub']             = { 1, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"2b2a2b","AccentColor":"797777","BackgroundColor":"202020","OutlineColor":"3f3a3f"}') },
+		['Mainhub']             = { 1, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"2b2a2b","AccentColor":"00ff7c","BackgroundColor":"202020","OutlineColor":"3f3a3f"}') },
 		['Default'] 		= { 2, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1c1c1c","AccentColor":"0055ff","BackgroundColor":"141414","OutlineColor":"323232"}') },
 		['Dracula'] 		= { 3, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"232533","AccentColor":"6271a5","BackgroundColor":"1b1c27","OutlineColor":"7c82a7"}') },
 		['Bitch Bot'] 		= { 4, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1e1e1e","AccentColor":"7e48a3","BackgroundColor":"232323","OutlineColor":"141414"}') },
